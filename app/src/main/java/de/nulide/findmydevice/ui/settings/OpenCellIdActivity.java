4958556be5e74f9908dde0becfd740aea8b831ad
Thore Goebel <goebel.thore@gmail.com>
2023-05-14 09:24:40 +0200

    Add button to paste API key from clipboard

--- app/src/main/java/de/nulide/findmydevice/ui/settings/OpenCellIdActivity.java
+++ app/src/main/java/de/nulide/findmydevice/ui/settings/OpenCellIdActivity.java
@@ -1,11 +1,16 @@
 package de.nulide.findmydevice.ui.settings;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import de.nulide.findmydevice.R;
 import de.nulide.findmydevice.data.Settings;
@@ -13,11 +18,13 @@ import de.nulide.findmydevice.data.io.IO;
 import de.nulide.findmydevice.data.io.JSONFactory;
 import de.nulide.findmydevice.data.io.json.JSONMap;
 
+
 public class OpenCellIdActivity extends AppCompatActivity implements TextWatcher {
 
     private Settings Settings;
 
     private EditText editTextOpenCellIdKey;
+    private Button buttonPaste;
 
 
     @Override
@@ -31,6 +38,8 @@ public class OpenCellIdActivity extends AppCompatActivity implements TextWatcher
         editTextOpenCellIdKey.setText((String) Settings.get(Settings.SET_OPENCELLID_API_KEY));
         editTextOpenCellIdKey.addTextChangedListener(this);
 
+        buttonPaste = findViewById(R.id.buttonPaste);
+        buttonPaste.setOnClickListener(this::onPasteClicked);
     }
 
     @Override
@@ -47,6 +56,15 @@ public class OpenCellIdActivity extends AppCompatActivity implements TextWatcher
     public void afterTextChanged(Editable edited) {
         if (edited == editTextOpenCellIdKey.getText()) {
             Settings.set(Settings.SET_OPENCELLID_API_KEY, edited.toString());
+        }
+    }
+
+    private void onPasteClicked(View view) {
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData.Item item = clipboardManager.getPrimaryClip().getItemAt(0);
+        CharSequence pasteData = item.getText();
+        if (pasteData != null) {
+            editTextOpenCellIdKey.setText(pasteData);
         }
     }
 }
