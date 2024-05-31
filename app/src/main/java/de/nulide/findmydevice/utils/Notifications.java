cfe55ae3f059dcb840580bd8ac0372c39642a0e3
Thore Goebel <goebel.thore@gmail.com>
2024-05-31 21:54:34 +0200

    Improve Notification Channel strings
    String cleanup. Also remove the unused CHANNEL_LIFECYCLE.

--- app/src/main/java/de/nulide/findmydevice/utils/Notifications.java
+++ app/src/main/java/de/nulide/findmydevice/utils/Notifications.java
@@ -19,20 +19,17 @@ import de.nulide.findmydevice.ui.MainActivity;
 public class Notifications {
 
     public static final int CHANNEL_USAGE = 42;
-    public static final int CHANNEL_LIFE = 43;
+    // public static final int CHANNEL_LIFE = 43;
     public static final int CHANNEL_PIN = 44;
     public static final int CHANNEL_SERVER = 45;
-
     public static final int CHANNEL_SECURITY = 46;
-
     public static final int CHANNEL_FAILED = 47;
 
     private static boolean silent;
 
-
     public static void notify(Context context, String title, String text, int channelID) {
         if (!silent) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, new Integer(channelID).toString())
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, Integer.valueOf(channelID).toString())
                     .setSmallIcon(R.drawable.ic_notification)
                     .setContentTitle(title)
                     .setContentText(text)
@@ -47,7 +44,8 @@ public class Notifications {
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
 
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                Logger.log(title, text);
+                Logger.log("Notifications", "Cannot send notification: missing permission POST_NOTIFICATIONS");
+                Logger.log("Notifications", title + ": " + text);
                 return;
             }
 
@@ -58,22 +56,19 @@ public class Notifications {
     public static void init(Context context, boolean silentWish) {
         silent = silentWish;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel1 = new NotificationChannel(new Integer(CHANNEL_USAGE).toString(), context.getString(R.string.Notification_Usage), NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel1 = new NotificationChannel(Integer.valueOf(CHANNEL_USAGE).toString(), context.getString(R.string.Notification_Usage), NotificationManager.IMPORTANCE_DEFAULT);
             channel1.setDescription(context.getString(R.string.Notification_Usage_Description));
-            NotificationChannel channel2 = new NotificationChannel(new Integer(CHANNEL_LIFE).toString(), context.getString(R.string.Notification_Lifecycle), NotificationManager.IMPORTANCE_DEFAULT);
-            channel2.setDescription(context.getString(R.string.Notification_Lifecycle_Description));
-            NotificationChannel channel3 = new NotificationChannel(new Integer(CHANNEL_PIN).toString(), context.getString(R.string.Pin_Usage), NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel3 = new NotificationChannel(Integer.valueOf(CHANNEL_PIN).toString(), context.getString(R.string.Pin_Usage), NotificationManager.IMPORTANCE_DEFAULT);
             channel3.setDescription(context.getString(R.string.Notification_Pin_Usage_Description));
-            NotificationChannel channel4 = new NotificationChannel(new Integer(CHANNEL_SERVER).toString(), context.getString(R.string.Notification_Server), NotificationManager.IMPORTANCE_DEFAULT);
-            channel4.setDescription(context.getString(R.string.NotificationServer_Description));
-            NotificationChannel channel5 = new NotificationChannel(new Integer(CHANNEL_SECURITY).toString(), context.getString(R.string.Notification_Security), NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel4 = new NotificationChannel(Integer.valueOf(CHANNEL_SERVER).toString(), context.getString(R.string.Notification_Server), NotificationManager.IMPORTANCE_DEFAULT);
+            channel4.setDescription(context.getString(R.string.Notification_Server_Description));
+            NotificationChannel channel5 = new NotificationChannel(Integer.valueOf(CHANNEL_SECURITY).toString(), context.getString(R.string.Notification_Security), NotificationManager.IMPORTANCE_DEFAULT);
             channel5.setDescription(context.getString(R.string.Notification_Security_Description));
-            NotificationChannel channel6 = new NotificationChannel(new Integer(CHANNEL_FAILED).toString(), context.getString(R.string.Notification_FAIL), NotificationManager.IMPORTANCE_HIGH);
+            NotificationChannel channel6 = new NotificationChannel(Integer.valueOf(CHANNEL_FAILED).toString(), context.getString(R.string.Notification_FAIL), NotificationManager.IMPORTANCE_HIGH);
             channel6.setDescription(context.getString(R.string.Notification_Fail_Description));
 
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel1);
-            notificationManager.createNotificationChannel(channel2);
             notificationManager.createNotificationChannel(channel3);
             notificationManager.createNotificationChannel(channel4);
             notificationManager.createNotificationChannel(channel5);
