988698aa9aba1ea1eab9c6eccfb46869734606cd
Thore Goebel <goebel.thore@gmail.com>
2024-11-25 09:19:18 +0100

    Refactor: convert AppUpdatedReceiver + BootReceiver to Kotlin

---
+++ app/src/main/java/de/nulide/findmydevice/receiver/AppUpdatedReceiver.kt
@@ -0,0 +1,37 @@
+package de.nulide.findmydevice.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import de.nulide.findmydevice.data.SettingsRepository
+import de.nulide.findmydevice.services.FMDServerLocationUploadService
+import de.nulide.findmydevice.services.TempContactExpiredService
+import de.nulide.findmydevice.ui.onboarding.UpdateboardingModernCryptoActivity
+import de.nulide.findmydevice.utils.log
+
+
+class AppUpdatedReceiver : BroadcastReceiver() {
+
+    companion object {
+        private val TAG: String = AppUpdatedReceiver::class.java.simpleName
+
+        const val APP_UPDATED: String = "android.intent.action.MY_PACKAGE_REPLACED"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent) {
+        val settings = SettingsRepository.Companion.getInstance(context!!)
+
+        if (intent.action == APP_UPDATED) {
+            context.log().i(TAG, "Running MY_PACKAGE_REPLACED (APP_UPDATED) handler")
+
+            TempContactExpiredService.scheduleJob(context, 0)
+
+            settings.migrateSettings()
+            UpdateboardingModernCryptoActivity.notifyAboutCryptoRefreshIfRequired(context)
+
+            if (settings.serverAccountExists()) {
+                FMDServerLocationUploadService.scheduleJob(context, 0)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/de/nulide/findmydevice/receiver/BootReceiver.kt
@@ -0,0 +1,43 @@
+package de.nulide.findmydevice.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import de.nulide.findmydevice.data.Settings
+import de.nulide.findmydevice.data.SettingsRepository
+import de.nulide.findmydevice.services.FMDServerLocationUploadService
+import de.nulide.findmydevice.services.FmdBatteryLowService
+import de.nulide.findmydevice.services.TempContactExpiredService
+import de.nulide.findmydevice.ui.onboarding.UpdateboardingModernCryptoActivity
+import de.nulide.findmydevice.utils.log
+
+
+class BootReceiver : BroadcastReceiver() {
+
+    companion object {
+        private val TAG: String = BootReceiver::class.java.simpleName
+
+        const val BOOT_COMPLETED: String = "android.intent.action.BOOT_COMPLETED"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent) {
+        val settings = SettingsRepository.Companion.getInstance(context!!)
+
+        if (intent.action == BOOT_COMPLETED) {
+            context.log().i(TAG, "Running BOOT_COMPLETED handler")
+
+            TempContactExpiredService.scheduleJob(context, 0)
+
+            if (settings.get(Settings.SET_FMD_LOW_BAT_SEND) as Boolean) {
+                FmdBatteryLowService.scheduleJobNow(context)
+            }
+
+            UpdateboardingModernCryptoActivity.notifyAboutCryptoRefreshIfRequired(context)
+
+            if (settings.serverAccountExists()) {
+                FMDServerLocationUploadService.scheduleJob(context, 0)
+                PushReceiver.registerWithUnifiedPush(context)
+            }
+        }
+    }
+}
